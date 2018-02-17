7ffc7019a125a819c6a3042f46f1481608e9ab73
Олесуа и Юля <astahova.98@gmail.com>
2018-02-17 12:29:42 +0700

    Вызов членов интерфейсов на уровне объектов

---
+++ CustomInterface/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With Interfaces *****\n");
+           
+            /// Вызывает свойствоPoints, определенное интерфейсом IPointy.
+            Hexagon hex = new Hexagon();
+            Console.WriteLine("Points: {0}", hex.Points);
+            
+            // Перехватить возможность исключение InvalidCastException.
+            Circle c = new Circle("Lisa");
+            IPointy itfPt = null;
+            try
+            {
+                itfPt = (IPointy)c;
+                Console.WriteLine(itfPt.Points);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CustomInterface/Shape.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    abstract class Shape
+    {
+        public Shape(string name = "NoName")
+        { PetName = name; }
+        public string PetName { get; set; }
+
+        //единственный виртуальный метод
+        public virtual void Draw()
+        {
+            Console.WriteLine("Inside Shape.Draw()");
+        }
+        
+    }
+    class Circle : Shape
+    {
+        public Circle() { }
+        public Circle(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Circle", PetName);
+        }
+    }
+
+    class Hexagon : Shape, IPointy
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Hexagon", PetName);
+        }
+        // Реализация IPointy.
+        public byte Points
+        {
+            get { return 3; }
+        }
+    }
+
+    class Triangle : Shape, IPointy
+    {
+        public Triangle() { }
+        public Triangle(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Tringle", PetName);
+        }
+         // Реализация IPointy.
+        public byte Points
+        {
+            get { return 3;  }
+        }
+    }
+}
